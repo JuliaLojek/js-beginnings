a23d90cb2874afb83d2d6c3590b22e7d6d2a7507
Julia Lojek <julialojek@gmail.com>
2020-01-12 14:04:32 +0100

    links.hyml and links.js added

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-alert("Hello, world!");
+var youLikeMeat = false;
+
+if(youLikeMeat){
+    document.write("Here is the meaty menu...");
+} else{
+    document.write("Here is the vegetarian menu");
+}
+
+var age = 5;
+
+while(age < 10){
+    document.write("You are less than 10");
+    // console.log("You are less than 10");
+    age++;
+}
+document.write("Youe are 10 or over");
+
+for(age=2; age < 10; age++){
+    console.log("You are under 10");
+}
+document.write("Wow");
