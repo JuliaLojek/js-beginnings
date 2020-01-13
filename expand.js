f54a4a2915ead15075aaeeb8fcef83f8e291cfdc
Julia Lojek <julialojek@gmail.com>
2020-01-13 18:12:48 +0100

    discoBox element added and styled in div-expand.html, style.css, expand.js

--- expand.js
+++ expand.js
@@ -1,17 +1,47 @@
-var article = document.getElementById("content");
+function setUpEvents(){
 
-var button = document.getElementById("show-more");
+    var article = document.getElementById("content");
+    var button = document.getElementById("show-more");
 
-// button.onclick = function(){
-//     article.setAttribute("class", "open");
-// };
+    // button.onclick = function(){
+    //     article.setAttribute("class", "open");
+    // };
 
-button.onclick = function(){
-    if(article.className == "open"){
-        article.className = "";
-        button.textContent = "Show more";
-    } else{
-        article.className = "open";
-        button.textContent = "Show less";
+    button.onclick = function(){
+        if(article.className == "open"){
+            article.className = "";
+            button.textContent = "Show more";
+        } else{
+            article.className = "open";
+            button.textContent = "Show less";
+        }
+    };
+}
+
+window.onload = function(){
+    setUpEvents();
+};
+
+
+
+
+
+var discoBox = document.getElementById("disco");
+var colors = ["crimson", "deepPink", "gold", "lightSeaGreen", "midnightBlue", "seaGreen", "pink", "indigo", "aqua", "salmon"];
+var counter = 0;
+
+function colorChange(){
+    if(counter >= colors.length){
+        counter = 0;
     }
+
+    discoBox.style.backgroundColor = colors[counter];
+    counter++;
+}
+
+var myTimer = setInterval(colorChange, 50);
+
+discoBox.onclick = function(){
+    clearInterval(myTimer);
+    discoBox.textContent = "This is your color!";
 };
